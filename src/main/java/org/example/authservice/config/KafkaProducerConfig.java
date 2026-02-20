cb33a57962c9e26652337be6f01113f1bcc44ab0
AlionaVr <Alonavershilo@yandex.by>
2026-02-20 17:08:59 +0300

    Refactor Kafka integration with Spring Kafka
    - Replace manual Kafka producer setup with Spring Kafka configuration.
    - Refactor `VerificationCodeProducer` to use `KafkaTemplate`.
    - Update application properties for Spring Kafka settings.
    - Remove redundant Apache Kafka dependencies.

--- src/main/java/org/example/authservice/config/KafkaProducerConfig.java
+++ src/main/java/org/example/authservice/config/KafkaProducerConfig.java
@@ -1,31 +1,31 @@
 package org.example.authservice.config;
 
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
+import org.example.authservice.dto.event.SendCodeEvent;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 
-import java.util.Properties;
+import java.util.Map;
 
 @Configuration
+@RequiredArgsConstructor
 public class KafkaProducerConfig {
 
-    @Bean(destroyMethod = "close")
-    public KafkaProducer<String, String> kafkaProducer(
-            @Value("${kafka.bootstrap-servers}") String bootstrapServers
-    ) {
-        Properties props = new Properties();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+    private final KafkaProperties kafkaProperties;
 
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+    @Bean
+    public ProducerFactory<String, SendCodeEvent> producerFactory() {
+        Map<String, Object> configProps = kafkaProperties.buildProducerProperties();
 
-        props.put(ProducerConfig.ACKS_CONFIG, "all");
-        props.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
-        props.put(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
 
-        return new KafkaProducer<>(props);
+    @Bean
+    public KafkaTemplate<String, SendCodeEvent> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
     }
 }
